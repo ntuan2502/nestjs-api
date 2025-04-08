7abf1d36d568f3fbb2a4f1a3b8b12548d70e056f
Tún <ntuan.2502@gmail.com>
2025-04-08 14:54:10 +0700

    feat(office): update CreateOfficeDto with new optional fields

--- src/offices/dto/create-office.dto.ts
+++ src/offices/dto/create-office.dto.ts
@@ -1,11 +1,23 @@
-import { IsString, MinLength } from 'class-validator';
+import { IsString, MinLength, IsOptional } from 'class-validator';
 
 export class CreateOfficeDto {
   @IsString({ message: 'Name must be a string' })
   @MinLength(1, { message: 'Name must not be empty' })
   name: string;
 
+  @IsString({ message: 'International name must be a string' })
+  @IsOptional()
+  internationalName?: string;
+
+  @IsString({ message: 'Short name must be a string' })
+  @IsOptional()
+  shortName?: string;
+
+  @IsString({ message: 'Tax code must be a string' })
+  @MinLength(1, { message: 'Tax code must not be empty' })
+  taxCode: string;
+
   @IsString({ message: 'Address must be a string' })
-  @MinLength(1, { message: 'Address must not be empty' })
-  address: string;
+  @IsOptional()
+  address?: string;
 }
